cb9e8bbe268bd6e011fce53c491a0b6b42362e76
mihran <mihran.eprem@gmail.com>
2021-11-20 23:34:55 -0300

    creating project

---
+++ src/main/java/com/micro/beerProject/beerProject/BeerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.micro.beerProject.beerProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/micro/beerProject/beerProject/BeerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.beerProject.beerProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
